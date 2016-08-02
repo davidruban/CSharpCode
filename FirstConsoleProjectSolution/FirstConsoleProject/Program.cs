c9fac420bb0806bf0b64f257e363956b52b35ffe
youpunk9 <davidruban9@gmail.com>
2016-08-01 20:37:37 -0700

    Add files via upload
    My Current State of my C# code.

---
+++ FirstConsoleProjectSolution/FirstConsoleProject/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace FirstConsoleProject
+{
+	class MainClass
+	{
+		public static void Main (string[] args)	//This is a method called "Main". It is called when the program starts.
+		{
+            CreateGrid();
+
+            Console.ReadKey ();
+
+        }
+        public static void CreateGrid ()
+        {
+            int width = 20;
+            int height = 20;
+
+            int[][] grid = new int[width][height];
+
+            }
+        }
+
+	}
+}
